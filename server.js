f6c8d2d19bcba4aff00a4f54ba0d3936a172e1e7
Sergio <sergiomattei@outlook.com>
2016-03-12 23:26:17 -0400

    Added latitude and location, individual sensor API, now 	- All using mockup data until hardware arrives. 	- Latlong TODO

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/status', function (req, res) {
  * 
  * @return {string} [JSON containing current information]
  */
-app.get('/current', function(req, res) {
-	sensor_data = {
+app.get('/now', function(req, res) {
+	var sensor_data = {
 		temperature_f: 20,
 		temperature_c: 20,
 		humidity: 10,
@@ -46,7 +46,42 @@ app.get('/current', function(req, res) {
 	res.send(JSON.stringify(sensor_data));
 });
 
+/**
+ * Returns raw sensor data.
+ * @return {string} [JSON containing sensor data.]
+ */
+app.get('/sensors/:sensor', function(req, res) {
+	var sensor = req.params.sensor;
+	switch (sensor) {
+		case 'temperature':
+			data = 20;
+			break;
+		case 'humidity':
+			data = 10;
+			break;
+		case 'pressure':
+			data = 10;
+			break;
+	}
 
+	sensor_data = {
+		data: data,
+		time: Date.now(),
+		success: true
+	}
+
+	// return the json
+	res.send(JSON.stringify(sensor_data));
+});
+
+/**
+ * Returns current sensor location.
+ * @return {string} [JSON containing sensor data.]
+ */
+app.get('/location', function(req, res) {
+	// TODO
+	res.send("latlong");
+});
 
 
 app.listen(8080, function () {
