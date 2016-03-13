9d36700f6f3875ab7bbae72590767a209e206d6e
Sergio <sergiomattei@outlook.com>
2016-03-12 21:17:18 -0400

    Created current JSON route 	- JSON route for current statistics. 	- This will be used for storage and archival node. 	- And fronted sometimes.

--- server.js
+++ server.js
@@ -10,20 +10,43 @@
 var express = require('express');
 var app = express();
 
-/*
-Set status route.
-This dictates whether the server is running or not, more of a dev/test route.
-If a sensor fails initializing this will return it.
-*/
+/**
+ * Server and sensor status probe. 
+ * @return {string} [JSON containing status information]  
+ */
+
 app.get('/status', function (req, res) {
 	var status = {
-		'server': true,
-		'sensors': true,
-		'success': true
+		server: true,
+		sensors: true,
+		success: true
 	}
 	// return the stringified json
   res.send(JSON.stringify(status));
 });
+
+/**
+ * Returns current information from sensors in JSON format.
+ * A preffered method to access current data is using sockets for each
+ * sensor, but sometimes in the frontend this is needed.
+ * 
+ * @return {string} [JSON containing current information]
+ */
+app.get('/current', function(req, res) {
+	sensor_data = {
+		temperature_f: 20,
+		temperature_c: 20,
+		humidity: 10,
+		pressure: 10,
+		time: Date.now(),
+		success: true
+	};
+
+	// return the json
+	res.send(JSON.stringify(sensor_data));
+});
+
+
 
 
 app.listen(8080, function () {
